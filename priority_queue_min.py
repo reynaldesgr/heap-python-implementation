69b9a06bca6a58c81d5736f265562e8fa3dc6af4
reynaldesgr <100307961+reynaldesgr@users.noreply.github.com>
2022-05-12 15:52:30 +0200

    Add files via upload

---
+++ priority_queue_min.py
@@ -0,0 +1,42 @@
+import heap_min
+
+""" return the max of the priority file"""
+def get_min_heap(heap):
+    return heap[0]
+
+""" extract the max from the heap and return (heap, max)"""
+def extract_min_from_heap(heap, length):
+    if length < 1:
+        exit()
+    min = get_min_heap(heap)
+    heap[0] = heap[length - 1]
+    heap = heap[:len(heap) - 1]
+    heap_min.cram_min(heap, 0, len(heap))
+    return heap, max
+
+""" decrease the key at index i """
+def decrease_heap_key(heap, i, key):
+    if key < heap[i]:
+        exit()
+    heap[i] = key
+    while i >= 0 and heap[heap_min.parent(i)] > heap[i]:
+        heap[heap_min.parent(i)], heap[i] = heap[i], heap[heap_min.parent(i)]
+        i = heap_min.parent(i)
+    return heap
+
+""" increase the key at index i """
+def increase_heap_key(heap, i, key):
+    if key < heap[i]:
+        exit()
+    heap[i] = key
+    heap_min.cram_min(heap, i, len(heap))
+    return heap
+
+def insert_into_heap(A, key):
+    length = len(A) + 1
+    A.append(-float('inf'))
+    decrease_heap_key(A, len(A) - 1, key)
+
+heap            = [2, 5, 19, 20, 15, 45, 6]
+insert_into_heap(heap, 18)
+print(heap)
